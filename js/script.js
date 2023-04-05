dbb193ef4b9a587168c217c65544f463f8d82f81
Jean-Marc18 <pich@gmail.com>
2023-04-05 00:13:46 +0000

    contact part

--- js/script.js
+++ js/script.js
@@ -39,3 +39,21 @@ var typed = new Typed(".multiple-text", {
     backDelay: 1000,
     loop: true
 })
+
+
+// contact input
+
+const inputs = document.querySelectorAll(".contact-input");
+
+inputs.forEach((ipt) => {
+    ipt.addEventListener('focus', () => {
+        ipt.parentNode.classList.add("focus")
+        ipt.parentNode.classList.add("not-empty")
+    })
+    ipt.addEventListener('blur', () => {
+        ipt.parentNode.classList.remove("focus")
+        if (ipt.value == "") {
+            ipt.parentNode.classList.remove("not-empty")
+        }
+    })
+})
